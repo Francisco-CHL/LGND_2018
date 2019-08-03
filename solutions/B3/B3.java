3b96f2c581307729d22626806d9faa380594e562
Francisco-CHL <ogalde.francisco@gmail.com>
2019-08-03 19:05:48 -0400

    B3 Check

--- solutions/B3/B3.java
+++ solutions/B3/B3.java
@@ -4,18 +4,19 @@ public class B3 {
     public static void main(String[] args) {
         // Mr. Magoo
         Scanner in = new Scanner(System.in);
-        String input = "{1,2,3,4},{5,6},{5,6},{7,8,9,10,11,12},{13}";
+        String input = in.nextLine();
+        in.close();
+        // String input = "{1,2,3,4},{5,6},{5,6},{7,8,9,10,11,12},{13}";
         input = input.replace("},{", "}-{");
         String[] s = input.split("-");
         int n = 0;
         String output = "";
-        // String input = in.nextLine():
 
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s.length; j++) {
                 if (i != j) {
                     if (s[i].equals(s[j])) {
-                        s[i] = "";
+                        s[j] = "";
                     }
                 }
             }
@@ -23,11 +24,12 @@ public class B3 {
 
         for (int i = 0; i < s.length; i++) {
             if (s[i] != "") {
-                output = s[i];
+                output += s[i];
+                n++;
             }
-
         }
+        output = output.replace("}{", "},{");
+        System.out.print(n + output);
         System.out.println("");
-        in.close();
     }
 }
