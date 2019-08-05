e914d8bc2e2d1fe8f98dc3ec19970c800b0defee
Francisco-CHL <ogalde.francisco@gmail.com>
2019-08-04 22:17:25 -0400

    C1

--- solutions/C1/C1.java
+++ solutions/C1/C1.java
@@ -4,10 +4,29 @@ public class C1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // String input = in.nextLine();
-        String input = ".-..  ---...  *--.  .  -.  ..  ---...  *-.  ---*-..  ..-.  .-.  --.  -. ";
+        String input = ".-.. --- ... *--. . -. .. --- ... *-. --- *-.. ..- . .-. -- . -.";
+        // String input = ".... --- .-.. .-";
         String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
-                "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
-                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----" };
+                "-.", "--.--", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+                ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----" };
 
+        in.close();
+        String[] abc = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+        String output = "";
+        String[] msge = input.split("\\*");
+        for (int i = 0; i < msge.length; i++) {
+            String[] word = msge[i].split(" ");
+            for (int j = 0; j < word.length; j++) {
+                for (int k = 0; k < morse.length; k++) {
+                    if (word[j].equals(morse[k])) {
+                        output += abc[k];
+                        break;
+                    }
+                }
+            }
+            output += " ";
+        }
+        System.out.println(output.trim().toUpperCase());
     }
 }
