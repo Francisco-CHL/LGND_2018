ab02d536f5424a89758c2fc574d13406cb7a2887
Francisco-CHL <ogalde.francisco@gmail.com>
2019-08-12 19:19:11 -0400

    Se agrega C4

---
+++ solutions/C4/C4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class C4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+    }
+}
